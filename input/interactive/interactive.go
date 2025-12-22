df08c11c2dee0a266cf2953d8bae153398049c85
Nemessiah <andecase@gmail.com>
2025-12-22 14:37:16 -0700

    Add package and move Prompt to it.

---
+++ input/interactive/interactive.go
@@ -0,0 +1,34 @@
+package interactive
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Prompt[T any](prompt string) T {
+	reader := bufio.NewReader(os.Stdin)
+	var zero T // default zero value of type T
+
+	for {
+		fmt.Print(prompt)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		switch any(zero).(type) {
+		case int:
+			val, err := strconv.Atoi(input)
+			if err != nil {
+				fmt.Println("Invalid entry, must be an integer.")
+				continue
+			}
+			return any(val).(T)
+		case string:
+			return any(input).(T)
+		default:
+			panic("unsupported type")
+		}
+	}
+}
