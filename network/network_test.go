6e5de9ab17f4c3fa2ce20f7fe8a44fa57cd08bac
Nemessiah <andecase@gmail.com>
2026-01-16 22:32:03 -0700

    Finish Tests

--- network/network_test.go
+++ network/network_test.go
@@ -62,3 +62,57 @@ func TestSelectInterface(t *testing.T) {
 		})
 	}
 }
+
+func TestGetFirstUsableCIDR(t *testing.T) {
+	tests := []struct {
+		name      string
+		cidr      string
+		want      string
+		wantErr   bool
+		errString string
+	}{
+		{
+			name: "Basic",
+			cidr: "10.3.42.0/24",
+			want: "10.3.42.1/24",
+		},
+		{
+			name: "Basic_2",
+			cidr: "10.3.42.10/29",
+			want: "10.3.42.9/29",
+		},
+		{
+			name:      "Invalid_CIDR",
+			cidr:      "10.3.42.10_29",
+			wantErr:   true,
+			errString: "invalid CIDR",
+		},
+		{
+			name:      "Invalid_NetworkSize",
+			cidr:      "10.3.42.0/32",
+			wantErr:   true,
+			errString: "network too small, no usable host IPs",
+		},
+		{
+			name:      "IPv6",
+			cidr:      "fe80::88e8:5802:2d14:c001/64",
+			wantErr:   true,
+			errString: "only IPv4 is supported",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetFirstUsableCIDR(tt.cidr)
+
+			if err == nil && tt.wantErr {
+				if tt.errString != "" && !strings.Contains(err.Error(), tt.errString) {
+					t.Fatalf("error = %q; want substring %q", err.Error(), tt.errString)
+				}
+			}
+			if !strings.Contains(got, tt.want) {
+				t.Fatalf("got %#v; want %#v", got, tt.want)
+			}
+		})
+	}
+}
