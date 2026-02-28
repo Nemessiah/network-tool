3f364a9552cd13af1fba6597c8315f2674f6fbf2
Nemessiah <andecase@gmail.com>
2026-02-28 16:01:35 -0700

    Revert "change background to gray" main back to stable point This reverts commit fc209ef9ec4116e5bf70495977c13c67728d2dfc.

--- go.mod
+++ go.mod
@@ -3,14 +3,3 @@ module github.com/nemessiah/network-tool
 go 1.25.4
 
 require gopkg.in/yaml.v3 v3.0.1
-
-require (
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.8.0 // indirect
-	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	github.com/kr/pretty v0.3.0 // indirect
-	golang.org/x/sync v0.17.0 // indirect
-	golang.org/x/text v0.29.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-)
--- main.go
+++ main.go
@@ -1,37 +1,23 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-	"time"
 
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/nemessiah/network-tool/entrypoint/api"
 	"github.com/nemessiah/network-tool/entrypoint/interactive"
 	"github.com/nemessiah/network-tool/internal"
 )
 
 func main() {
-	mode := flag.String("mode", "cli", "Run mode (e.g. cli, api)")
-	config := flag.String("config", "local", "Where to generate config from")
+	mode := flag.String("mode", "cli", "Run mode (e.g. dev, prod, test)")
 	flag.Parse()
 
-	switch *config {
-	case "":
-		fmt.Println("Error: --config is required")
-		os.Exit(1)
-	case "local":
-		fmt.Println("Loading config from home directory")
-		err := internal.GenerateConfig()
-		if err != nil {
-			log.Fatalf("Error (%T): %+v", err, err)
-		}
-	default:
-		fmt.Println("Invalid config:", *config)
-		os.Exit(1)
+	err := internal.GenerateConfig()
+	if err != nil {
+		log.Fatalf("Error (%T): %+v", err, err)
 	}
 
 	switch *mode {
@@ -41,18 +27,8 @@ func main() {
 	case "cli":
 		interactive.CliEntry()
 	case "api":
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-
-		dbpool, err := pgxpool.New(ctx, os.Getenv("DATABASE_URL"))
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
-			os.Exit(1)
-		}
-		defer dbpool.Close()
-
 		fmt.Println("Running in mode:", *mode)
-		api.ApiEntry(context, dbpool)
+		api.ApiEntry()
 	default:
 		fmt.Println("Invalid mode:", *mode)
 		os.Exit(1)
